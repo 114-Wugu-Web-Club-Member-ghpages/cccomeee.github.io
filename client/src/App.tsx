b2dff72667eda18a885905f2cbffa50dc390816e
KutsunaSubaRya <index20010928@gmail.com>
2026-01-02 13:59:58 +0800

    refactor!: convert to pure frontend static site
    Remove all backend functionality (server/, shared/, database configs) and dependencies to enable static site deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Navigation from "@/components/Navigation";
@@ -10,9 +8,6 @@ import Insights from "@/pages/Insights";
 import Diary from "@/pages/Diary";
 import Tutorials from "@/pages/Tutorials";
 import Contact from "@/pages/Contact";
-import Admin from "@/pages/Admin";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
 import NotFound from "@/pages/not-found";
 
 function Router() {
@@ -23,9 +18,6 @@ function Router() {
       <Route path="/diary" component={Diary} />
       <Route path="/tutorials" component={Tutorials} />
       <Route path="/contact" component={Contact} />
-      <Route path="/admin" component={Admin} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -33,18 +25,16 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <div className="min-h-screen flex flex-col">
-          <Navigation />
-          <main className="flex-1">
-            <Router />
-          </main>
-          <Footer />
-        </div>
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <div className="min-h-screen flex flex-col">
+        <Navigation />
+        <main className="flex-1">
+          <Router />
+        </main>
+        <Footer />
+      </div>
+      <Toaster />
+    </TooltipProvider>
   );
 }
 
